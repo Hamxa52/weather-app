63c5705ae8973452b7012de9301c1b93569c8bb5
Hamza-1608 <92798422+Hamxa52@users.noreply.github.com>
2024-12-07 16:19:33 +0500

    Updates

---
+++ precache-manifest.8435b484068568b452c9bb1df6dbe444.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d3ab1c9731c41dd2e4b100f83fda32",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "05a1b9b21544b28de096",
+    "url": "/weather-app/static/css/main.ea928825.chunk.css"
+  },
+  {
+    "revision": "6dcfc77f0f18ba424a25",
+    "url": "/weather-app/static/js/2.701fe897.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather-app/static/js/2.701fe897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a1b9b21544b28de096",
+    "url": "/weather-app/static/js/main.c142bbf3.chunk.js"
+  },
+  {
+    "revision": "2e2a4590687fd03295eb",
+    "url": "/weather-app/static/js/runtime-main.721937bf.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weather-app/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/weather-app/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/weather-app/static/media/city.e1698332.jpg"
+  }
+]);
